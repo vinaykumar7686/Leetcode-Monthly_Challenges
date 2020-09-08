a0b2bf1755ee4f9dac8c31c7257f168c99a33994
Vinay <vinaykumarvk.8866@gmail.com>
2020-09-08 21:57:53 +0530

    Recursive Solution to Sum of Root To Leaf Binary Numbers

--- September/Week2/Sum of Root To Leaf Binary Numbers.py
+++ September/Week2/Sum of Root To Leaf Binary Numbers.py
@@ -77,3 +77,19 @@ class IterativeSolution:
                         
         return root_to_leaf
         
+class RecursiveSolution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        def preorder(r, curr_number):
+            nonlocal root_to_leaf
+            if r:
+                curr_number = (curr_number << 1) | r.val
+                # if it's a leaf, update root-to-leaf sum
+                if not (r.left or r.right):
+                    root_to_leaf += curr_number
+                    
+                preorder(r.left, curr_number)
+                preorder(r.right, curr_number) 
+        
+        root_to_leaf = 0
+        preorder(root, 0)
+        return root_to_leaf
